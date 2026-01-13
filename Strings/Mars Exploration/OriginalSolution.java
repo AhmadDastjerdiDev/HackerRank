39956c9928e60149910a05255e0e7a25ccbb416a
Ahmad Dastjerdi <ahmaddastjerdi.eng@gmail.com>
2026-01-13 15:29:52 +0000

    Fix loop condition in marsExploration method

--- Strings/Mars Exploration/OriginalSolution.java
+++ Strings/Mars Exploration/OriginalSolution.java
@@ -1,7 +1,7 @@
     public static int marsExploration(String s) {
     // Write your code here
         int numberOfDifference = 0;
-        for(int i =0; i< (s.length()+1)/3;i+=3){
+        for(int i =0; i< s.length();i+=3){
             if(!s.substring(i, i+2).equalsIgnoreCase("SOS")){
                    if(s.charAt(i)!= 'S')
                         numberOfDifference++;
